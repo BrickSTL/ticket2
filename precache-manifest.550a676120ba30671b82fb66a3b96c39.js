d8b9d9e6c925cb167f1d28eeb8a47c448638deda
Ben Merrill <bendmerrill@gmail.com>
2018-12-18 18:15:55 -0600

    Updates

---
+++ precache-manifest.550a676120ba30671b82fb66a3b96c39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9738694086c38982637e",
+    "url": "/BrickSTL/ticket2/static/js/runtime~main.97386940.js"
+  },
+  {
+    "revision": "3c2ca95a5668cd3e2ce6",
+    "url": "/BrickSTL/ticket2/static/js/main.3c2ca95a.chunk.js"
+  },
+  {
+    "revision": "2cc47f83a56e8f8c0b8c",
+    "url": "/BrickSTL/ticket2/static/js/1.2cc47f83.chunk.js"
+  },
+  {
+    "revision": "3c2ca95a5668cd3e2ce6",
+    "url": "/BrickSTL/ticket2/static/css/main.6c040ba3.chunk.css"
+  },
+  {
+    "revision": "475ff7186ce4b09e37290edd060c1145",
+    "url": "/BrickSTL/ticket2/index.html"
+  }
+];
